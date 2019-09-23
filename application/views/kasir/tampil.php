836514f8ac2b08cf09dbfc5ca56671061de6eb8e
zalramli <github.little@gmail.com>
2019-09-23 21:21:17 +0700

    kasir proses

--- application/views/kasir/tampil.php
+++ application/views/kasir/tampil.php
@@ -2,7 +2,7 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-8 col-md-7 col-sm-7 col-xs-12">
-				<div style="overflow-y: scroll; height:550px; width: auto;" class="contact-inner">
+				<div style="margin-bottom:20px;overflow-y: scroll; height:550px; width: auto;" class="contact-inner">
 					<div class="row">
 						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 							<div style="margin-bottom:10px;" class="input-group">
@@ -58,8 +58,50 @@
 								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
+						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+							<div class="thumbnail">
+								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
+								<div class="caption">
+									<h5>Thumbnail label</h5>
+									<p>Rp 100.000</p>
+									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
+												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
+								</div>
+							</div>
+						</div>
+						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+							<div class="thumbnail">
+								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
+								<div class="caption">
+									<h5>Thumbnail label</h5>
+									<p>Rp 100.000</p>
+									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
+												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
+								</div>
+							</div>
+						</div>
+						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+							<div class="thumbnail">
+								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
+								<div class="caption">
+									<h5>Thumbnail label</h5>
+									<p>Rp 100.000</p>
+									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
+												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
+								</div>
+							</div>
+						</div>
+						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
+							<div class="thumbnail">
+								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
+								<div class="caption">
+									<h5>Thumbnail label</h5>
+									<p>Rp 100.000</p>
+									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
+												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
+								</div>
+							</div>
+						</div>
 						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
 							<div class="thumbnail">
 								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
@@ -105,61 +147,85 @@
 							</div>
 						</div>
 					</div>
-					<div class="row">
-						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-							<div class="thumbnail">
-								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
-								<div class="caption">
-									<h5>Thumbnail label</h5>
-									<p>Rp 100.000</p>
-									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
-												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
-								</div>
-							</div>
-						</div>
-						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-							<div class="thumbnail">
-								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
-								<div class="caption">
-									<h5>Thumbnail label</h5>
-									<p>Rp 100.000</p>
-									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
-												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
-								</div>
-							</div>
-						</div>
-						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-							<div class="thumbnail">
-								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
-								<div class="caption">
-									<h5>Thumbnail label</h5>
-									<p>Rp 100.000</p>
-									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
-												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
-								</div>
-							</div>
-						</div>
-						<div style="margin-bottom:5px;" class="col-lg-3 col-md-4 col-sm-6 col-xs-6">
-							<div class="thumbnail">
-								<img src="../assets/template/img/dialog/like.png" width="170" alt="...">
-								<div class="caption">
-									<h5>Thumbnail label</h5>
-									<p>Rp 100.000</p>
-									<p class="text-center"><a href="#" class="btn btn-primary" role="button"><i
-												class="glyphicon glyphicon-shopping-cart"></i> Beli</a></p>
-								</div>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 			<div class="col-lg-4 col-md-5 col-sm-5 col-xs-12">
 				<div class="contact-inner">
-					<div class="nk-int-mk sl-dp-mn sm-res-mg-t-10">
-						<h2>Cari Mekanik</h2>
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<h4 class="text-center">Keranjang Belanja</h4>
+							<div class="table-responsive">
+								<table width="100%" class="table">
+									<thead>
+										<tr>
+											<th class="text-center" width="68%">NAMA</th>
+											<th class="text-center" width="30%">HARGA</th>
+											<th width="2%">QTY</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Asus X453 MA</td>
+											<td class="text-right">3.500.000</td>
+											<td class="text-center">1</td>
+										</tr>
+										<tr>
+											<td>Acer X453 MA</td>
+											<td class="text-right">3.500.000</td>
+											<td class="text-center">2</td>
+										</tr>
+										<tr>
+											<td>Lenovo X453 MA</td>
+											<td class="text-right">3.500.000</td>
+											<td class="text-center">1</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
 					</div>
-
 				</div>
+				<div style="margin-top:20px;" class="contact-inner">
+					<div class="row">
+						<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+							<h4 class="text-center">Pembayaran</h4>
+							<div style="padding-left:20px;padding-right:20px;" class="row">
+								<form class="form-horizontal">
+									<div class="form-group">
+										<label for="inputEmail3" class="col-sm-2 control-label">Total</label>
+										<div class="col-sm-10">
+											<input type="email" class="form-control text-right" id="inputEmail3"
+												placeholder="Email" value="10.500.000" readonly>
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="inputEmail3" class="col-sm-2 control-label">Bayar</label>
+										<div class="col-sm-10">
+											<input type="email" class="form-control text-right" id="inputEmail3"
+												placeholder="Masukan jumlah pembayaran">
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="inputEmail3" class="col-sm-2 control-label">Kembali</label>
+										<div class="col-sm-10">
+											<input type="email" class="form-control text-right" id="inputEmail3"
+												placeholder="Email" value="500.000" readonly>
+										</div>
+									</div>
+									<div class="form-group">
+										<label for="inputEmail3" class="col-sm-2 control-label"></label>
+										<div class="col-sm-10 text-right">
+											<button type="submit" class="btn btn-primary">Simpan</button>
+											<a href="" class="btn btn-success">Print</a>
+
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+
 			</div>
 		</div>
 	</div>
