e2aa7754772b151bbbe7121ad8faef653969074b
zalramli <github.little@gmail.com>
2019-09-27 14:05:45 +0700

    home manager dan pimpinan

--- application/views/_partial/header.php
+++ application/views/_partial/header.php
@@ -1,19 +1,21 @@
 <div class="header-top-area">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+			<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
 				<div class="logo-area">
-					<h2 style="color:white;">LCC KOMPUTER</h2>
+					<h2 style="color:white;">INVENTORY MANAGEMENT</h2>
 				</div>
 			</div>
-			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
+			<div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
 				<div class="header-top-menu">
 					<ul class="nav navbar-nav notika-top-nav">
 						<li class="nav-item">
 							<a href="#" class="nav-link dropdown-toggle"><span>USERNAME (Hak Akses)</span></a>
 						</li>
 						<li class="nav-item">
-							<a href="#" class="nav-link dropdown-toggle" data-toggle="modal" data-target="#myModaltwo"><span><i class="fa fa-power-off" aria-hidden="true"></i></span></a>
+							<a href="#" class="nav-link dropdown-toggle" data-toggle="modal"
+								data-target="#myModaltwo"><span><i class="fa fa-power-off"
+										aria-hidden="true"></i></span></a>
 						</li>
 
 						<div class="modal fade" id="myModaltwo" role="dialog">
--- application/views/pimpinan/home/tampil.php
+++ application/views/pimpinan/home/tampil.php
@@ -1,58 +1,105 @@
 <div class="breadcomb-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="breadcomb-list">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="breadcomb-wp">
-                                <div class="breadcomb-icon">
-                                    <i class="notika-icon notika-form"></i>
-                                </div>
-                                <div class="breadcomb-ctn">
-                                    <h2>Dashboard</h2>
-                                    <p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
-                            <div class="breadcomb-report">
-                                <button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="notika-icon notika-sent"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="breadcomb-list">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+							<div class="breadcomb-wp">
+								<div class="breadcomb-icon">
+									<i class="notika-icon notika-form"></i>
+								</div>
+								<div class="breadcomb-ctn">
+									<h2>Dashboard</h2>
+									<p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
+							<div class="breadcomb-report">
+								<button data-toggle="tooltip" data-placement="left" title="Download Report"
+									class="btn"><i class="notika-icon notika-sent"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- Breadcomb area End-->
+<div class="data-table-area">
+	<div class="container">
+		<div class="row" style="margin-bottom:27px;">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+				<select class="form-control">
+					<option>Semua Toko</option>
+					<option>LCC Komputer</option>
+					<option>CMC Komputer</option>
+					<option>Toko Alat Tulis</option>
+					<option>Toko Property</option>
+				</select>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+				<select class="form-control">
+					<option>HARI INI</option>
+					<option>MINGGU INI</option>
+					<option>BULAN INI</option>
+				</select>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row">
+		<a style="color:black" href="">
+			<div style="margin-bottom: 30px;" class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-inner">
+					<div class="contact-inner">
+						<h2 class="text-right">
+							67
+						</h2>
+						<span><strong>JUMLAH TRANSAKSI</strong></span>
+					</div>
+				</div>
+			</div>
+		</a>
+		<a style="color:black" href="">
+			<div style="margin-bottom: 30px;" class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-inner">
+					<div class="contact-inner">
+						<h2 class="text-right">
+							Rp 900.0000
+						</h2>
+						<span><strong>LABA</strong></span>
+					</div>
+				</div>
+			</div>
+		</a>
+
+		<a style="color:black" href="">
+			<div style="margin-bottom: 30px;" class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+				<div class="contact-inner">
+					<div class="contact-inner">
+						<h2 class="text-right">
+							Rp 100.0000
+						</h2>
+						<span><strong>RUGI</strong></span>
+					</div>
+				</div>
+			</div>
+		</a>
+	</div>
+</div>
 
 <div class="line-chart-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <div class="line-chart-wp chart-display-nn">
-                    <canvas height="140vh" width="180vw" id="basiclinechart"></canvas>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <div class="line-chart-wp sm-res-mg-t-30 chart-display-nn">
-                    <canvas height="140vh" width="180vw" id="linechartinterpolation"></canvas>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <div class="line-chart-wp mg-t-30 chart-display-nn">
-                    <canvas height="140vh" width="180vw" id="linechartstyles"></canvas>
-                </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <div class="line-chart-wp mg-t-30 chart-mg-nt">
-                    <canvas height="140vh" width="180vw" id="linechartpointcircle"></canvas>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="line-chart-wp chart-display-nn">
+					<canvas height="50vh" width="90vw" id="basiclinechart"></canvas>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
