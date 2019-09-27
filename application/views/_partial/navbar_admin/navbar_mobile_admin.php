4b0ae0f07a76b9655a536c54695e04b6bb410688
zalramli <github.little@gmail.com>
2019-09-27 14:49:33 +0700

    --

--- application/views/_partial/navbar_admin/navbar_mobile_admin.php
+++ application/views/_partial/navbar_admin/navbar_mobile_admin.php
@@ -15,17 +15,17 @@
 									<li><a href="<?= base_url() ?>admin/jenis_toko">Jenis Toko</a></li>
 								</ul>
 							</li>
+							<li><a data-toggle="collapse" data-target="#distributor" href="#">Distributor</a>
+								<ul id="distributor" class="collapse dropdown-header-top">
+									<li><a href="<?= base_url() ?>admin/distributor">Data Distributor</a></li>
+								</ul>
+							</li>
 							<li><a data-toggle="collapse" data-target="#democrou" href="#">Data Barang</a>
 								<ul id="democrou" class="collapse dropdown-header-top">
 									<li><a href="<?= base_url() ?>admin/barang_global">Barang</a></li>
 									<li><a href="<?= base_url() ?>admin/merk_barang">Merk</a></li>
 									<li><a href="<?= base_url() ?>admin/kategori_barang">Kategori</a></li>
 									<li><a href="<?= base_url() ?>admin/satuan_barang">Satuan</a></li>
-								</ul>
-							</li>
-							<li><a data-toggle="collapse" data-target="#distributor" href="#">Distributor</a>
-								<ul id="distributor" class="collapse dropdown-header-top">
-									<li><a href="<?= base_url() ?>admin/distributor">Data Distributor</a></li>
 								</ul>
 							</li>
 						</ul>
